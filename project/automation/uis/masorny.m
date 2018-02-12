b01ff424212ec76493c10824a97f32d73b4b825e
liubobo <60231857@qq.com>
2018-02-12 10:28:05 +0800

    加入xib解析第二版优化代码，之前的暂时不改动

---
+++ project/automation/uis/masorny.m
@@ -0,0 +1,28 @@
+{{props}}
+
+- (void)p_layoutSubViews
+{
+    
+{{layoutSubViews}}
+    [self p_updateSubViews];
+
+}
+
+#pragma mark - # delegate
+{{delegate}}
+
+#pragma mark - # Event Response
+{{touches}}
+#pragma mark - # Private Methods
+- (void)p_layoutSubViews
+{
+ {{masonrys}}
+}
+
+#pragma mark - # Getter
+{{sums}}                
+
+
+
+
+@end
