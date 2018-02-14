a983f4e39384d9140270b502e56ab941b5b76f34
liubobo <60231857@qq.com>
2018-02-14 11:03:59 +0800

    优化props

--- project/automation/prop.py
+++ project/automation/prop.py
@@ -2,15 +2,25 @@
 from util import *
 from viewLists import vList as availables
 
-for line in readlines_from_stdin():
+def getProps(rline):
     rline = str(line).strip('\n').strip(' ')
-    splitList = filter(lambda m:len(m.strip(' ')), re.split(r'[;,\s]\s*', rline))
-
+    splitList = filter(lambda m: len(m.strip(' ')), re.split(r'[;,\s]\s*', rline))
     if len(splitList) >= 2:
-        print '@property(nonatomic, strong) {} *{};'.format(splitList[0], splitList[1])
+        stype = splitList[0]
+        if stype == 's':
+            stype = 'NSSTring'
+        if stype == 'a':
+            stype = 'NSArray'
+        if stype == 'd':
+            stype = 'NSDictionary'
+
+        return '@property(nonatomic, strong) {} *{};'.format(stype, splitList[1])
     else:
         try:
-            vtype = filter(lambda v:rline.endswith(v.replace('UI', '')), availables)[0]
-            print '@property(nonatomic, strong) {} *{};'.format(vtype, rline)
+            vtype = filter(lambda v: rline.endswith(v.replace('UI', '')), availables)[0]
+            return '@property(nonatomic, strong) {} *{};'.format(vtype, rline)
         except:
-            print '@property(nonatomic, strong) {} *{};'.format('<#type#>', rline)
+            return '@property(nonatomic, strong) {} *{};'.format('<#type#>', rline)
+
+for line in readlines_from_stdin():
+    print  getProps(line)
