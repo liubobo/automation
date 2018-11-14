a0adb731199a3541c75e341b3dc74830eea787d5
liubobo <602318557@qq.com>
2018-11-14 15:51:12 +0800

    改进排序功能

--- project/automation/ATool/sortcode.py
+++ project/automation/ATool/sortcode.py
@@ -2,8 +2,52 @@
 __author__ = 'liubo'
 from Util import *
 
+def sortbyLen(lines):
+
+    lines.sort(key=lambda x: len(x))  # a b c ,last len
+    if len(lines) == 0:
+        return
+    map(print_obj, lines)
+    print '\n'+' '
+
+
+def sortImport(lines):
+    arr_import = []
+    arr_include = []
+    arr_third = []
+    arr_category = []
+
+    for line  in lines:
+          if '+' in line:
+              arr_category.append(line)
+              continue
+          if '<' in line:
+              arr_third.append(line)
+              continue
+          if '#import' in line:
+              arr_import.append(line)
+              continue
+          if '#include' in line:
+               arr_include.append(line)
+               continue
+
+    sortbyLen(arr_import)
+    sortbyLen(arr_category)
+    sortbyLen(arr_include)
+    sortbyLen(arr_third)
+
 def sortcode():
+
     lines = readlines_from_stdin()
-    lines.sort(key=lambda x: len(x))
-    map(print_obj,lines)
+    joinedStr = ''.join(lines) 
+
+    if '#import' in joinedStr or '#include' in joinedStr:
+        sortImport(lines)
+    else:
+        lines.sort(key=lambda x: len(x)) #a b c ,last len
+        map(print_obj,lines)
+
+
+
+
 
